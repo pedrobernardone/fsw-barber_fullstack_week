f65e2b0a514820ad69583b2319ec5f470508aec4
Pedro Bernardoni <pedrobernardoni56@gmail.com>
2026-08-18 23:54:42 -0300

    feat: add header and header components and a banner

---
+++ app/_components/ui/header.tsx
@@ -0,0 +1,19 @@
+import { Card, CardContent } from "@/app/_components/ui/card";
+import { Button } from "@/app/_components/ui/button";
+import Image from "next/image";
+import { MenuIcon } from "lucide-react";
+
+const Header = () => {
+  return (
+    <Card>
+      <CardContent className="gflex items-center justify-between">
+        <Image alt="fsw barber" src="/logo.png" width={120} height={18} />
+        <Button variant="outline" size="icon">
+          <MenuIcon />
+        </Button>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default Header;
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,38 @@
 "use client";
 
-import { useState } from "react";
-import { Button } from "./_components/ui/button";
+import Header from "./_components/ui/header";
+import { Input } from "@/app/_components/ui/input";
+import { Button } from "@/app/_components/ui/button";
+import { SearchIcon } from "lucide-react";
+import Image from "next/image";
 
 // SERVER COMPONENTS WHEN NO USE CLIENT SET
 const Home = () => {
-  return <Button>Pronto para testar</Button>;
+  return (
+    <div>
+      <Header />
+      <div className="p-5">
+        <h2 className="text-2xl font-bold">Olá, Pedro!</h2>
+        <p>Terça feira, 18 de Agosto</p>
+
+        <div className="flex items-center gap-2 mt-6">
+          <Input placeholder="Faça sua busca aqui..." />
+          <Button>
+            <SearchIcon />
+          </Button>
+        </div>
+
+        <div className="relative w-full h-[150px] mt-6">
+          <Image
+            alt="banner"
+            src="/banner-pizza.png"
+            fill
+            className="object-cover"
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
